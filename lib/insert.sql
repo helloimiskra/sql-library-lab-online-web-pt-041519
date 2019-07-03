b877aa10fca6ec5f51707300e527d57ac91162d5
Iskra B <ibatistapoblete@gmail.com>
2019-07-03 03:14:32 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -22,6 +22,8 @@ INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES
 (1, "Harry Potter", "The Boy Who Lived", "Wizard", 1, 1),
 (2, "Dobby", "Dobby is a free elf.", "Elf", 1, 1),
 (3, "Petunia Dursley", "I was the only one who saw her for what she was... a freak!", "Muggle", 1, 1),
-(4, "Bella", "I'm going to be 19 forever.", "Human", 2, 2),
-(5, "Edward", "I watched you while you were sleeping.", "Vampire", 2, 2),
-(6, "Jacob", "Sorry I cannot be the right monster for you.", "Werewolf", 2, 2);
+(4, "Hermione", "Honestly, am I the only person who's ever bothered to read Hogwarts, A History?", "Wizard", 1, 1),
+(5, "Bella", "I'm going to be 19 forever.", "Human", 2, 2),
+(6, "Edward", "I watched you while you were sleeping.", "Vampire", 2, 2),
+(7, "Victoria", "I'm the one with the wicked curve ball.", "Vampire", 2, 2),
+(8, "Jacob", "Sorry I cannot be the right monster for you.", "Werewolf", 2, 2);
