d16f42b1c1340e859b387d73244b1335ebe42099
Iskra B <ibatistapoblete@gmail.com>
2019-07-03 02:53:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,11 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES
 (1, 'Harry Potter', 1, 1),
 (2, 'Twilight', 2, 2);
+
+INSERT INTO books (id, title, year, series_id) VALUES
+(1, "Harry Potter and the Sorceror's Stone", 1997, 1),
+(2, "Harry Potter and the Chamber of Secrets", 1998, 1),
+(3, "Harry Potter and the Prisoner of Azkaban", 1999, 1),
+(4, "Twilight", 2005, 2),
+(5, "New Mood", 2006, 2),
+(6, "Eclipse", 2007, 2);
