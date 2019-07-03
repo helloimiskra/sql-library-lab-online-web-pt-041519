a7f78267c10daad12de7a300b82e9e5346b1baef
Iskra B <ibatistapoblete@gmail.com>
2019-07-03 02:35:53 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,6 +1,18 @@
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
-  author TEXT,
-  sub-genre TEXT
+  author_id TEXT,
+  sub_genre TEXT
 );
+
+CREATE TABLE sub_genre (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE books
