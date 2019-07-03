03b9fdd924f51280d83098b2bf7d2302717983d3
Iskra B <ibatistapoblete@gmail.com>
2019-07-03 04:35:14 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,10 @@ ORDER BY authors.name;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series
+JOIN characters 
+ON series.id = characters.series_id WHERE species = 'human'
+GROUP BY species;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
