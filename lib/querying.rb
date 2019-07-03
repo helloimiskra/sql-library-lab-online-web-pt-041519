81d6eeca584ab4759f250f62cd732412121aec71
Iskra B <ibatistapoblete@gmail.com>
2019-07-03 04:36:26 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,8 +23,8 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
-JOIN characters
-ON series.id = characters.series_id WHERE species = 'human'
+JOIN characters 
+ON series.id = characters.series_id WHERE species = "human"
 GROUP BY species;"
 end
 
